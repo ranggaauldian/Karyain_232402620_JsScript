ecfb0e41633b5114e7fa99c5d7aa8cf315d9537c
Vieeprmna <auldianrangga@gmail.com>
2025-07-14 09:53:24 +0700

    second commit

---
+++ belajar oop.js
@@ -0,0 +1,15 @@
+const Eko = {        // object
+    firstName:"Eko",
+    lastName: "khannedy"
+};
+
+const Budi = {      // object
+    firstName:"Budi",
+    lastName: "Nugraha"
+};
+
+const Joko = {      // object
+    firstName:"Joko",
+    lastName: "Saputra"
+};
+
